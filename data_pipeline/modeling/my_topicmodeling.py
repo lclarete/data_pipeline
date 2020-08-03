6720b13b00c9e9d8608ee4cbacd4389dd4fcc37a
lclarete <livia.clarete@gmail.com>
2020-08-03 10:20:47 -0400

    fixing types; putting class and topic modeling function in the same file -- easier to find all of them in the same place

--- data_pipeline/modeling/my_topicmodeling.py
+++ data_pipeline/modeling/my_topicmodeling.py
@@ -1,6 +1,5 @@
 """
-Implement topic modeling.
-First draft.
+Class and functions to perform topic modeling.
 
 Learn how to interpret the results from topic modeling.
 What is:
@@ -21,12 +20,18 @@ What is:
 - Describe the process and algoritm used
 - Include sources
 
+Topic Modeling with LSA, PLSA, LDA & lda2Vec:
+https://medium.com/nanonets/topic-modeling-with-lsa-psla-lda-and-lda2vec-555ff65b0b05
+
+Topic modeling with NMF and SVD:
+http://localhost:8888/notebooks/course-nlp/2-svd-nmf-topic-modeling.ipynb
+
+
 https://shichaoji.com/tag/topic-modeling-python-lda-visualization-gensim-pyldavis-nltk/
 https://github.com/bmabey/pyLDAvis/blob/master/notebooks/GraphLab.ipynb
 https://github.com/bmabey/pyLDAvis/blob/master/notebooks/pyLDAvis_overview.ipynb
 
 https://github.com/bmabey/pyLDAvis
-
 
 """
 
@@ -42,9 +47,86 @@ import pyLDAvis.gensim
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# import preprocessing pipeline
+from data_pipeline.pre_processing_text.class_preprocessing import nlp_preprocessor
 
 # tokenize a list
 from data_pipeline.pre_processing_text.split_ngrams import tokenize_list
+
+
+class topic_modeling_nlp:
+
+    def __init__(self, model, preprocessing_pipeline=None):
+        """
+        Pipeline for topic modeling.
+        Uses a default preprocessing class
+        """
+
+        self.model = model
+
+        self._is_fit = False
+
+        if not preprocessing_pipeline:
+            self.preprocessor = nlp_preprocessor()
+        else:
+            self.preprocessor = preprocessing_pipeline
+
+
+    def fit(self, X):
+        """
+        Train the vectorizer and model together using the 
+        input training data.
+        """
+        self.preprocessor.fit(X)
+        train_data = self.preprocessor.transform(X)
+        self.model.fit(train_data)
+        self._is_fit = True
+
+
+    def transform(self, X):
+        """
+        Predicts on the data provided.
+        """
+        if not self._is_fit:
+            raise ValueError("Must fit the model before transforming!")
+        test_data = self.preprocessor.transform(X)
+        preds = self.model.transform(test_data)
+        return preds
+
+
+    def print_topics(self, num_words=10):
+        """
+        Print out the top words for each topic
+        """
+        feat_names = self.preprocessor.vectorizer.get_feature_names()
+
+        for topic_idx, topic in enumerate(self.model.components_):
+            message = 'Topic #%d' % topic_idx
+            message += " ".join([feat_names[i] for i in topic.argsort()[: -num_words - 1:-1]])
+            
+            print(message)
+
+    
+    def save_pipe(self, filename):
+        """
+        Save the pipeline.
+        """
+        if type(filename) != str:
+            raise TypeError("Filename must be a string.")
+        pickle.dump(self.__dict__, open(filename+".mdl", "wb"))
+
+    def load_pipe(self, filename):
+        """
+        Load the pipeline.
+        """
+        if type(filename) != str:
+            raise TypeError("Filke must be a string.")
+        if filename[-4] != ".mdl":
+            filename += ".mdl"
+        self.__dict__ = pickle.load(open(filename, "rb"))
+
+
+
 
 
 def lda_topic_model(list_of_strings):
