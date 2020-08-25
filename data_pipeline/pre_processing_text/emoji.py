a9c9df711fb7083b650d50c634c1345b8a19387f
lclarete <livia.clarete@gmail.com>
2020-08-25 13:34:00 -0400

    apply the function above to retrieve dataframes, from emojis, images, sub_categs and categs

--- data_pipeline/pre_processing_text/emoji.py
+++ data_pipeline/pre_processing_text/emoji.py
@@ -9,9 +9,9 @@ https://advertools.readthedocs.io/en/master/advertools.emoji.html
 import advertools as ad
 import pandas as pd
 
-def return_df_count_emojis(list_of_strings, type_of_emoji='top_emoji_text', overview=True):
+def counting_emojis(list_of_strings, type_of_emoji='top_emoji_text', overview=False):
     """
-    Count the number of each emoji within a string 
+    Count the number of each emoji in a string 
     or a list of strings
     Input:
         list_of_strings: string or list of strings
@@ -23,11 +23,17 @@ def return_df_count_emojis(list_of_strings, type_of_emoji='top_emoji_text', over
             # categories
                 top_emoji_groups: description of emoji groups
                 top_emoji_sub_groups: description of emoji subgroups
+            
+            ## >> list of emoji's categories:
+            ## >> emojis_categ = ['top_emoji_text', 'top_emoji', 'top_emoji_groups', 'top_emoji_sub_groups']
                 
     Output:
         dataframe counting the number of each emoji
-        from the vocab
+            from the vocab
+        If the 'overview' parameter is equal True,
+            return a dataframe with metrics per post
     """
+
     # extract the emojis
     emoji_summary = ad.extract_emoji(list_of_strings)[type_of_emoji]
     # create a dataframe with the returned dictionary
@@ -45,4 +51,38 @@ def return_df_count_emojis(list_of_strings, type_of_emoji='top_emoji_text', over
     
     else:
         return df
+    
+
+
+## apply the function above to retrieve dataframes
+## from emojis, images, sub_categs and categs
+
+def retrieving_categ_emojis(list_of_strings):
+    """
+    Apply the counting_emojis function to retrieve 
+    the emoji classes, and subclasses
+    Input:
+        list_of_strings: string or list of strings
+    Output:
+        dataframe with three colums: emoji, n, and type
+    """
+    # list of categories
+    emojis_categ = ['top_emoji_text', 'top_emoji', 'top_emoji_groups', 'top_emoji_sub_groups']
+    
+    # create a dataframe
+    dfs = pd.DataFrame()
+    
+    # iterate over the categories 
+    for categ in emojis_categ:
+        # apply the function to count the number of emojis
+        emoji_dfs = return_df_count_emojis(df.tweet_full_text, 
+                                           type_of_emoji=categ,
+                                           overview=False)
+        # create a new column to describe the information retrieved
+        emoji_dfs['type'] = categ
+        # concat the datasets
+        dfs = pd.concat([dfs, emoji_dfs], axis=0)
+
+    # return the concated dataset    
+    return dfs
     
