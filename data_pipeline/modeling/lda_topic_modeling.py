4adaf3393e1f566d734b003cd29790adedf85209
lclarete <livia.clarete@gmail.com>
2020-09-25 16:57:04 -0400

    simple functions for the whole lda model pipeline: from preprocessing to visualization. The code come from a tutorial

---
+++ data_pipeline/modeling/lda_topic_modeling.py
@@ -0,0 +1,324 @@
+"""
+"""
+
+import sys
+import re, numpy as np, pandas as pd
+from collections import Counter
+
+from pprint import  pprint
+
+# Gensim
+import gensim, spacy, logging, warnings
+import gensim.corpora as corpora
+from gensim.utils import lemmatize, simple_preprocess
+from gensim.models import CoherenceModel
+
+# NLTK stop words
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get', 'do', 'done', 'try', 'many', 'some', 'nice', 'thank', 'think', 'see', 'rather', 'easy', 'easily', 'lot', 'lack', 'make', 'want', 'seem', 'run', 'need', 'even', 'right', 'line', 'even', 'also', 'may', 'take', 'come'])
+
+# viz
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.colors as mcolors
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+
+# format
+# warnings.filterwarnings('ignore', category=DeprecationWarning)
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+
+
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True) 
+        yield(sent)  
+        
+
+
+# building the bigram and trigram models
+def building_bi_tri_grams(data):
+    bigram = gensim.models.Phrases(data, 
+                               min_count=5, 
+                               # higher threshold fewer phrases
+                               threshold=100)
+
+    trigram = gensim.models.Phrases(bigram[data],
+                               threshold=100)
+
+    bigram_mod = gensim.models.phrases.Phraser(bigram)
+    trigram_mod = gensim.models.phrases.Phraser(trigram)
+    
+    return bigram_mod, trigram_mod
+
+
+# preprocess words
+def preprocess_words(texts, stop_words=stop_words, spacy_model='en_core_web_sm', allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """
+    Remove stopwords, form bigrams, trigrams and lemmatization
+    """
+    # create bigrams and trigrams
+    bigram_mod, trigram_mod = building_bi_tri_grams(texts)
+    
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    
+    texts_out = []
+    nlp = spacy.load(spacy_model, disable=['parser', 'ner'])
+    
+    for sent in texts:
+        doc = nlp(' '.join(sent))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]
+    
+    return texts_out
+
+# build Lda Model
+
+def build_id2word(data):
+    return corpora.Dictionary(data)
+
+def build_corpus(data):
+    # Create Dictionary
+    id2word = corpora.Dictionary(data)
+
+    # Create Corpus: Term Document Frequency
+    corpus = [id2word.doc2bow(text) for text in data]
+
+    return corpus
+
+
+# build Lda Model
+def build_lda_model(data, num_topics=4, alpha='symmetric'):
+    # Create Dictionary
+    id2word = corpora.Dictionary(data)
+
+    # Create Corpus: Term Document Frequency
+    corpus = [id2word.doc2bow(text) for text in data]
+
+    # Build LDA model
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                               id2word=id2word,
+                                               num_topics=num_topics, 
+                                               random_state=100,
+                                               update_every=1,
+                                               chunksize=10,
+                                               passes=10,
+                                               alpha=alpha,
+                                               iterations=100,
+                                               per_word_topics=True)
+    return lda_model
+
+
+# Dominant topic and its contribution in each document
+def format_topics_sentences(ldamodel, corpus, texts):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list            
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    
+    df_dominant_topic = sent_topics_df.reset_index()
+    df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+
+    return df_dominant_topic
+
+
+# Most representative sentence for each topic
+# Gets the most exemplar sentence for each topic.
+
+def sent_topics_sorted_df(df_topic_sents_keywords):
+    pd.options.display.max_colwidth = 100
+
+    sent_topics_sorteddf_mallet = pd.DataFrame()
+
+    sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+    for i, grp in sent_topics_outdf_grpd:
+        sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet,
+                                                 grp.sort_values(['Topic_Perc_Contrib'],
+                                                                 ascending=False).head(1)], axis=0)
+
+    # format
+    sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+    
+    return sent_topics_sorteddf_mallet
+
+
+# Frequency distribution of Word counts in Documents
+
+def frequency_distribution_word_counts(df_data):
+    doc_lens = [len(d) for d in df_data.Text]
+
+    # Plot
+    plt.figure(figsize=(16,7), dpi=160)
+    plt.hist(doc_lens, bins = 1000, color='navy')
+    plt.text(750, 100, "Mean   : " + str(round(np.mean(doc_lens))))
+    plt.text(750,  90, "Median : " + str(round(np.median(doc_lens))))
+    plt.text(750,  80, "Stdev   : " + str(round(np.std(doc_lens))))
+    plt.text(750,  70, "1%ile    : " + str(round(np.quantile(doc_lens, q=0.01))))
+    plt.text(750,  60, "99%ile  : " + str(round(np.quantile(doc_lens, q=0.99))))
+
+    plt.gca().set(xlim=(0, 1000), ylabel='Number of Documents', xlabel='Document Word Count')
+    plt.tick_params(size=16)
+    plt.xticks(np.linspace(0,1000,9))
+    plt.title('Distribution of Document Word Counts', fontdict=dict(size=22))
+    plt.show()
+
+
+
+def frequency_distribution_word_counts_per_topic(df_data):
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+    fig, axes = plt.subplots(2,2,figsize=(16,14), dpi=160, sharex=True, sharey=True)
+
+    for i, ax in enumerate(axes.flatten()):
+        df_dominant_topic_sub = df_data.loc[df_data.Dominant_Topic == i, :]
+        doc_lens = [len(d) for d in df_data.Text]
+        ax.hist(doc_lens, bins = 1000, color=cols[i])
+        ax.tick_params(axis='y', labelcolor=cols[i], color=cols[i])
+        sns.kdeplot(doc_lens, color="black", shade=False, ax=ax.twinx())
+        ax.set(xlim=(0, 1000), xlabel='Document Word Count')
+        ax.set_ylabel('Number of Documents', color=cols[i])
+        ax.set_title('Topic: '+str(i), fontdict=dict(size=16, color=cols[i]))
+
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.90)
+    plt.xticks(np.linspace(0,1000,9))
+    fig.suptitle('Distribution of Document Word Counts by Dominant Topic', fontsize=22)
+    plt.show()
+
+
+# Word clouds of Top N keywords in each topic
+
+def word_clouds_per_topic(lda_model):
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+    cloud = WordCloud(stopwords=stop_words,
+                      background_color='white',
+                      width=2500,
+                      height=1800,
+                      max_words=10,
+                      colormap='tab10',
+                      color_func=lambda *args, **kwargs: cols[i],
+                      prefer_horizontal=1.0)
+
+    topics = lda_model.show_topics(formatted=False)
+
+    fig, axes = plt.subplots(2, 2, figsize=(10,10), sharex=True, sharey=True)
+
+    for i, ax in enumerate(axes.flatten()):
+        fig.add_subplot(ax)
+        topic_words = dict(topics[i][1])
+        cloud.generate_from_frequencies(topic_words, max_font_size=300)
+        plt.gca().imshow(cloud)
+        plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+        plt.gca().axis('off')
+
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.axis('off')
+    plt.margins(x=0, y=0)
+    plt.tight_layout()
+    plt.show()
+
+
+
+# Sentence Coloring of N Sentences
+from matplotlib.patches import Rectangle
+
+def sentences_chart(lda_model, corpus, start = 0, end = 13):
+    corp = corpus[start:end]
+    mycolors = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+
+    fig, axes = plt.subplots(end-start, 1, figsize=(20, (end-start)*0.95), dpi=160)       
+    axes[0].axis('off')
+    for i, ax in enumerate(axes):
+        if i > 0:
+            corp_cur = corp[i-1] 
+            topic_percs, wordid_topics, wordid_phivalues = lda_model[corp_cur]
+            word_dominanttopic = [(lda_model.id2word[wd], topic[0]) for wd, topic in wordid_topics]    
+            ax.text(0.01, 0.5, "Doc " + str(i-1) + ": ", verticalalignment='center',
+                    fontsize=16, color='black', transform=ax.transAxes, fontweight=700)
+
+            # Draw Rectange
+            topic_percs_sorted = sorted(topic_percs, key=lambda x: (x[1]), reverse=True)
+            ax.add_patch(Rectangle((0.0, 0.05), 0.99, 0.90, fill=None, alpha=1, 
+                                   color=mycolors[topic_percs_sorted[0][0]], linewidth=2))
+
+            word_pos = 0.06
+            for j, (word, topics) in enumerate(word_dominanttopic):
+                if j < 14:
+                    ax.text(word_pos, 0.5, word,
+                            horizontalalignment='left',
+                            verticalalignment='center',
+                            fontsize=16, color=mycolors[topics],
+                            transform=ax.transAxes, fontweight=700)
+                    word_pos += .009 * len(word)  # to move the word for the next iter
+                    ax.axis('off')
+            ax.text(word_pos, 0.5, '. . .',
+                    horizontalalignment='left',
+                    verticalalignment='center',
+                    fontsize=16, color='black',
+                    transform=ax.transAxes)       
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.suptitle('Sentence Topic Coloring for Documents: ' + str(start) + ' to ' + str(end-2), fontsize=22, y=0.95, fontweight=700)
+    plt.tight_layout()
+    plt.show()
+
+
+def chart_word_counts_topic(data_processed, lda_model):
+    
+    topics = lda_model.show_topics(formatted=False)
+    data_flat = [w for w_list in data_processed for w in w_list]
+    counter = Counter(data_flat)
+
+    out = []
+    for i, topic in topics:
+        for word, weight in topic:
+            out.append([word, i , weight, counter[word]])
+
+    df = pd.DataFrame(out, columns=['word', 'topic_id', 'importance', 'word_count'])        
+
+    # Plot Word Count and Weights of Topic Keywords
+    fig, axes = plt.subplots(2, 2, figsize=(16,10), sharey=True, dpi=160)
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+    for i, ax in enumerate(axes.flatten()):
+        ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
+        ax_twin = ax.twinx()
+        ax_twin.bar(x='word', height="importance", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.2, label='Weights')
+        ax.set_ylabel('Word Count', color=cols[i])
+        ax_twin.set_ylim(0, 0.030); ax.set_ylim(0, 3500)
+        ax.set_title('Topic: ' + str(i), color=cols[i], fontsize=16)
+        ax.tick_params(axis='y', left=False)
+        ax.set_xticklabels(df.loc[df.topic_id==i, 'word'], rotation=30, horizontalalignment= 'right')
+        ax.legend(loc='upper left'); ax_twin.legend(loc='upper right')
+
+    fig.tight_layout(w_pad=2)    
+    fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=22, y=1.05)    
+    plt.show()
