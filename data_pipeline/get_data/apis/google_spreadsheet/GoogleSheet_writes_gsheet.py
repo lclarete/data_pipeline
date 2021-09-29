ae97154d075b8fdc908dae6f7ee6f9e84115996e
lclarete <livia.clarete@gmail.com>
2021-09-29 10:48:29 -0400

    from url to api_path in write_df_gsheet function

--- data_pipeline/get_data/apis/google_spreadsheet/GoogleSheet_writes_gsheet.py
+++ data_pipeline/get_data/apis/google_spreadsheet/GoogleSheet_writes_gsheet.py
@@ -1,12 +1,12 @@
 import pygsheets
 import pandas as pd
 
-def write_df_gsheet(sheet_name, tab_name, matrix_to_gsheet):
+def write_df_gsheet(sheet_name, tab_name, matrix_to_gsheet, api_path='~/src/apis_keys/googleKey_pesq7/client_secret.json'):
     # matrix_to_gsheet should contain no na values
-    # point the client_secret.json
-    url = '/home/livia/src/apis_keys/googleKey_pesq7/client_secret.json'
+    # api_path: point the client_secret.json 
+    
     # reading stopwords from a google sheet
-    gc = pygsheets.authorize(client_secret=url)
+    gc = pygsheets.authorize(client_secret=api_path)
     # Open spreadsheet and then workseet
     sh = gc.open(sheet_name)
     wks = sh.worksheet('title', tab_name)
